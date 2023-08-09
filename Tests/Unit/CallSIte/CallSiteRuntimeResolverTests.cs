6c3d34812e49352345a0d634b3746d170f666649
Celeste Marina Soueid <celeste@soueid.io>
2023-08-09 18:49:28 +1000

    test: add singleton and scoped dispose unit tests

--- Tests/Unit/CallSIte/CallSiteRuntimeResolverTests.cs
+++ Tests/Unit/CallSIte/CallSiteRuntimeResolverTests.cs
@@ -32,6 +32,31 @@ namespace Tests.Unit.CallSIte
             }
         }
 
+        public class ServiceProviderMock : IRootServiceProvider
+        {
+            public TService GetService<TService>() => throw new NotImplementedException();
+
+            public object GetService(Type type) => throw new NotImplementedException();
+
+            public Action Disposed { get; set; }
+            public ServiceProviderScope RootScope { get; }
+            public IServiceProviderScope CreateScope()
+            {
+                return new ServiceProviderScope(this, false);
+            }
+
+            public ServiceProviderMock()
+            {
+                RootScope = new ServiceProviderScope(this, true);
+            }
+
+            public void Dispose()
+            {
+                RootScope.Dispose();
+                Disposed?.Invoke();
+            }
+        }
+        
         private class IntService
         {
             public int Value { get; set; }
@@ -42,6 +67,11 @@ namespace Tests.Unit.CallSIte
             public string Value { get; set; }
         }
 
+        private class EmptyService
+        {
+            
+        }
+        
         private class Service
         {
             public IntService IntDep { get; }
@@ -54,20 +84,22 @@ namespace Tests.Unit.CallSIte
             }
         }
         
-        private ConstructorCallSite BuildConstructorCallSite(Type serviceType, ServiceCallSite[] paramCallSites = null)
+        private ConstructorCallSite BuildConstructorCallSite(Type serviceType, ServiceCallSite[] paramCallSites = null, 
+            ServiceLifetime lifetime = ServiceLifetime.Transient)
         {
-            var cacheInfo = new ServiceCacheInfo(ServiceLifetime.Transient, new ServiceIdentifier(serviceType));
+            var cacheInfo = new ServiceCacheInfo(lifetime, new ServiceIdentifier(serviceType));
             ConstructorInfo constructor = serviceType.GetConstructors()[0];
             return new ConstructorCallSite(cacheInfo, null, serviceType, constructor,
                 paramCallSites ?? Array.Empty<ServiceCallSite>());
         }
 
         [Fact]
-        public void ConstantCallSite()
+        public void ConstantResolution()
         {
+            var provider = new ServiceProviderMock();
             var resolver = new CallSiteRuntimeResolver(_ => new InjectorMock());
             var callSite = new ConstantCallSite(typeof(Service), new Service(new IntService(), new StringService()));
-            object serviceObject = resolver.Resolve(callSite, new ServiceProviderScope(null, true));
+            object serviceObject = resolver.Resolve(callSite, provider.RootScope);
             var service = (Service)serviceObject;
             Assert.IsType<Service>(serviceObject);
             Assert.NotNull(service);
@@ -76,8 +108,9 @@ namespace Tests.Unit.CallSIte
         }
         
         [Fact]
-        public void PrimaryConstructorCallSite()
+        public void PrimaryConstructorResolution()
         {
+            var provider = new ServiceProviderMock();
             var resolver = new CallSiteRuntimeResolver(_ => new InjectorMock());
 
             var intCallSite = new ConstantCallSite(typeof(IntService), new IntService());
@@ -86,7 +119,7 @@ namespace Tests.Unit.CallSIte
             ConstructorCallSite serviceCallSite =
                 BuildConstructorCallSite(typeof(Service), new ServiceCallSite[] { intCallSite, stringCallSite });
 
-            object serviceObject = resolver.Resolve(serviceCallSite, new ServiceProviderScope(null, true));
+            object serviceObject = resolver.Resolve(serviceCallSite, provider.RootScope);
             var service = (Service)serviceObject;
             Assert.IsType<Service>(serviceObject);
             Assert.NotNull(service);
@@ -95,8 +128,9 @@ namespace Tests.Unit.CallSIte
         }
 
         [Fact]
-        public void SecondaryConstructorCallSite()
+        public void SecondaryConstructorResolution()
         {
+            var provider = new ServiceProviderMock();
             var resolver = new CallSiteRuntimeResolver(_ => new InjectorMock());
 
             var intCallSite = BuildConstructorCallSite(typeof(IntService));
@@ -105,7 +139,7 @@ namespace Tests.Unit.CallSIte
             ConstructorCallSite serviceCallSite =
                 BuildConstructorCallSite(typeof(Service), new ServiceCallSite[] { intCallSite, stringCallSite });
 
-            object serviceObject = resolver.Resolve(serviceCallSite, new ServiceProviderScope(null, true));
+            object serviceObject = resolver.Resolve(serviceCallSite, provider.RootScope);
             Assert.IsType<Service>(serviceObject);
             var service = (Service)serviceObject;
             Assert.NotNull(service);
@@ -114,8 +148,9 @@ namespace Tests.Unit.CallSIte
         }
         
         [Fact]
-        public void PrimaryEnumerableCallSite()
+        public void PrimaryEnumerableResolution()
         {
+            var provider = new ServiceProviderMock();
             var resolver = new CallSiteRuntimeResolver(_ => new InjectorMock());
 
             var intCallSite = new ConstantCallSite(typeof(IntService), new IntService());
@@ -130,7 +165,7 @@ namespace Tests.Unit.CallSIte
             var enumerableCallSite = new EnumerableCallSite(enumerableCacheInfo, typeof(Service),
                 new ServiceCallSite[] { singleServiceCallSite, singleServiceCallSite });
 
-            object serviceObjects = resolver.Resolve(enumerableCallSite, new ServiceProviderScope(null, true));
+            object serviceObjects = resolver.Resolve(enumerableCallSite, provider.RootScope);
             
             Assert.IsAssignableFrom<IEnumerable<Service>>(serviceObjects);
             Service[] services = ((IEnumerable<Service>)serviceObjects).ToArray();
@@ -152,8 +187,9 @@ namespace Tests.Unit.CallSIte
         }
         
         [Fact]
-        public void SecondaryEnumerableCallSite()
+        public void SecondaryEnumerableResolution()
         {
+            var provider = new ServiceProviderMock();
             var resolver = new CallSiteRuntimeResolver(_ => new InjectorMock());
 
             ConstructorCallSite intCallSite = BuildConstructorCallSite(typeof(IntService));
@@ -165,7 +201,7 @@ namespace Tests.Unit.CallSIte
             ConstructorCallSite singleServiceCallSite =
                 BuildConstructorCallSite(typeof(EnumerableService), new ServiceCallSite[] { intEnumerableCallSite });
 
-            object serviceObject = resolver.Resolve(singleServiceCallSite, new ServiceProviderScope(null, true));
+            object serviceObject = resolver.Resolve(singleServiceCallSite, provider.RootScope);
             
             Assert.IsType<EnumerableService>(serviceObject);
             var service = (EnumerableService)serviceObject;
@@ -182,14 +218,15 @@ namespace Tests.Unit.CallSIte
         }
         
         [Fact]
-        public void PrimaryFactoryCallSite()
+        public void PrimaryFactoryResolution()
         {
+            var provider = new ServiceProviderMock();
             var resolver = new CallSiteRuntimeResolver(_ => new InjectorMock());
             
             FactoryCallSite serviceCallSite = BuildFactoryCallSite(typeof(Service),
-                provider => new Service(new IntService(), new StringService()));
+                p => new Service(new IntService(), new StringService()));
 
-            object serviceObject = resolver.Resolve(serviceCallSite, new ServiceProviderScope(null, true));
+            object serviceObject = resolver.Resolve(serviceCallSite, provider.RootScope);
             Assert.IsType<Service>(serviceObject);
             var service = (Service)serviceObject;
             Assert.NotNull(service);
@@ -198,22 +235,64 @@ namespace Tests.Unit.CallSIte
         }
 
         [Fact]
-        public void SecondaryFactoryCallSite()
+        public void SecondaryFactoryResolution()
         {
+            var provider = new ServiceProviderMock();
             var resolver = new CallSiteRuntimeResolver(_ => new InjectorMock());
 
-            FactoryCallSite intCallSite = BuildFactoryCallSite(typeof(IntService), provider => new IntService());
-            FactoryCallSite stringCallSite = BuildFactoryCallSite(typeof(StringService), provider => new StringService());
+            FactoryCallSite intCallSite = BuildFactoryCallSite(typeof(IntService), p => new IntService());
+            FactoryCallSite stringCallSite = BuildFactoryCallSite(typeof(StringService), p => new StringService());
 
             ConstructorCallSite serviceCallSite =
                 BuildConstructorCallSite(typeof(Service), new ServiceCallSite[] { intCallSite, stringCallSite });
 
-            object serviceObject = resolver.Resolve(serviceCallSite, new ServiceProviderScope(null, true));
+            object serviceObject = resolver.Resolve(serviceCallSite, provider.RootScope);
             Assert.IsType<Service>(serviceObject);
             var service = (Service)serviceObject;
             Assert.NotNull(service);
             Assert.NotNull(service.IntDep);
             Assert.NotNull(service.StringDep);
+        }
+        [Fact]
+        public void SingletonDispose()
+        {
+            var provider = new ServiceProviderMock();
+            var resolver = new CallSiteRuntimeResolver(_ => new InjectorMock());
+
+            var scope = new ServiceProviderScope(provider, false);
+            var secondScope = new ServiceProviderScope(provider, false);
+
+            ConstructorCallSite callSite = BuildConstructorCallSite(typeof(DisposableService), 
+                Array.Empty<ServiceCallSite>(), ServiceLifetime.Scoped);
+
+            var firstResolved = (DisposableService)resolver.Resolve(callSite, scope);
+            var differentScopeResolved = (DisposableService)resolver.Resolve(callSite, secondScope);
+            
+            scope.Dispose();
+            Assert.True(scope.IsDisposed);
+            Assert.True(firstResolved?.IsDisposed);
+            Assert.False(differentScopeResolved?.IsDisposed);
+        }
+        
+        [Fact]
+        public void ScopedDispose()
+        {
+            var provider = new ServiceProviderMock();
+            var resolver = new CallSiteRuntimeResolver(_ => new InjectorMock());
+
+            var scope = new ServiceProviderScope(provider, false);
+            var secondScope = new ServiceProviderScope(provider, false);
+
+            ConstructorCallSite callSite = BuildConstructorCallSite(typeof(DisposableService), 
+                Array.Empty<ServiceCallSite>(), ServiceLifetime.Scoped);
+
+            var firstResolved = (DisposableService)resolver.Resolve(callSite, scope);
+            var differentScopeResolved = (DisposableService)resolver.Resolve(callSite, secondScope);
+            
+            scope.Dispose();
+            Assert.True(scope.IsDisposed);
+            Assert.True(firstResolved?.IsDisposed);
+            Assert.False(differentScopeResolved?.IsDisposed);
         }
     }
 }
