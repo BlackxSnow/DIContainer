52c18e4b417a14e953ea033c7795720f044dabad
Celeste Marina Soueid <celeste@soueid.io>
2023-08-14 18:58:03 +1000

    docs: document ICallSiteFactory.cs

--- DIContainer/CallSite/ICallSiteFactory.cs
+++ DIContainer/CallSite/ICallSiteFactory.cs
@@ -2,8 +2,16 @@ using DIContainer.Service;
 
 namespace DIContainer.CallSite
 {
+    /// <summary>
+    /// Provides retrieval of <see cref="ServiceCallSite">ServiceCallSites</see> by <see cref="ServiceIdentifier">ServiceIdentifiers</see>.
+    /// </summary>
     internal interface ICallSiteFactory
     {
+        /// <summary>
+        /// Attempt to retrieve the <see cref="ServiceCallSite"/> represented by <paramref name="identifier"/>.
+        /// </summary>
+        /// <param name="identifier">The identifying key for the desired <see cref="ServiceCallSite"/>.</param>
+        /// <returns>The call site on success, or null on failure.</returns>
         ServiceCallSite? GetCallSite(ServiceIdentifier identifier);
     }
 }
