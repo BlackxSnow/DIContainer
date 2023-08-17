443335a535e066dc51b835f16fa29b5aac53fc11
Celeste Marina Soueid <celeste@soueid.io>
2023-08-17 15:44:01 +1000

    docs: add IServiceProvider summary

--- DIContainer/Provider/IServiceProvider.cs
+++ DIContainer/Provider/IServiceProvider.cs
@@ -2,6 +2,9 @@ using System;
 
 namespace DIContainer.Provider
 {
+    /// <summary>
+    /// Provides retrieval of previously registered services by type.
+    /// </summary>
     public interface IServiceProvider
     {
         TService? GetService<TService>();
